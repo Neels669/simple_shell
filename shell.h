0d4e326da0302d990694a5d452e8540526f3a3d8
Neelam <neelam_669@yahoo.com>
2023-07-16 06:03:24 -0700

    Opening

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_HDR
+#define SHELL_HDR
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+
+#endif
