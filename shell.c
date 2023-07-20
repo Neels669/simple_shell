9ce67101eb2a28df4fe4dc2bed5e798b5e8a005b
Neelam <neelam_669@yahoo.com>
2023-07-20 08:01:53 -0700

    phew shell in interatctive mode

--- shell.c
+++ shell.c
@@ -36,44 +36,85 @@ void _EOF(int len, char *buff)
 }
 
 /**
- * _isatty - Function to dispaly the prompt
+ * execute_command - Execute external commands
+ * @args: Array of arguments
+ * @envp: Environment variables
  *
- * Return: void
+ * Return: 1 on success
  */
-void _isatty(void)
+int execute_command(char **args, char **envp)
 {
-	if (isatty(STDIN_FILENO))
-		printString("($) ");
+	pid_t child_pid;
+	int status;
+
+	child_pid = fork();
+	if (child_pid == 0)
+	{
+		if (execve(args[0], args, envp) == -1)
+		{
+			perror("execve error");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else if (child_pid < 0)
+	{
+		perror("fork error");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		waitpid(child_pid, &status, 0);
+	}
+	return (1);
 }
 
 /**
- * main - Command to pass the function
- * @ac: Argument count
- * @av: Argument vector
+ * main - Shell start
+ * @ac: Count in av
+ * @av: Array of strings
+ * @envp: NULL terminated array of strings
  *
- * Return: Status
+ * Return: prompt
  */
-int main(int ac, char **av)
+int main(int ac, char **av, char **envp)
 {
 	char *buffer = NULL;
 	size_t buffer_size = 0;
 	ssize_t line_len = 0;
+	char *token;
+	const char *delimiters = " \t\n";
+	char *args[64];
+	int arg_index = 0;
 
 	(void)ac;
 	(void)av;
 
 	signal(SIGINT, command_sign);
 
-	_isatty();
-
 	while (1)
 	{
+		printString("($) ");
+
 		line_len = getline(&buffer, &buffer_size, stdin);
 		if (line_len == -1)
 			_EOF(line_len, buffer);
-		_isatty();
-	}
 
+		token = strtok(buffer, delimiters);
+		while (token != NULL)
+		{
+			args[arg_index] = token;
+			arg_index++;
+
+			token = strtok(NULL, delimiters);
+		}
+		args[arg_index] = NULL;
+
+		if (strcmp(args[0], "exit") == 0)
+		{
+			_EOF(-1, buffer);
+		}
+		execute_command(args, envp);
+	}
 	free(buffer);
 	return (0);
 }
